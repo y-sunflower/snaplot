2818ced23fc91547aa39cb93b68871c294818099
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-10-02 14:52:38 +0200

    doc ok

--- snaplot/__init__.py
+++ snaplot/__init__.py
@@ -2,6 +2,7 @@ from .main import Camera
 import matplotlib
 
 # set non-interactive backend for Windows
+# https://github.com/y-sunflower/snaplot/issues/11
 matplotlib.use("Agg")
 
 __version__ = "0.1.0"
